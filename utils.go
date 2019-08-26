33e7832a813a66305ceb1ca3b6c651c618a04868
TheSecEng <zachary.schulze@gmail.com>
2019-08-26 12:06:44 -0400

    Added Error Struct for Capturing NMAP Errors. Utils for Removing Dupl. Utils file

---
+++ utils.go
@@ -0,0 +1,18 @@
+package nmap
+
+// https://siongui.github.io/2018/04/14/go-remove-duplicates-from-slice-or-array/
+func RemoveDuplicatesFromSlice(s []string) []string {
+	m := make(map[string]bool)
+	for _, item := range s {
+		if _, ok := m[item]; ok {
+		} else {
+			m[item] = true
+		}
+	}
+
+	var result []string
+	for item, _ := range m {
+		result = append(result, item)
+	}
+	return result
+}
