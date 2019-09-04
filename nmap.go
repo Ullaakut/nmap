ad451e52fec56c91d2d793f1876d679cf9e7a069
TheSecEng <zachary.schulze@gmail.com>
2019-09-04 15:13:02 -0400

    fixed nmap.go import

--- nmap.go
+++ nmap.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	. "github.com/ullaakut/nmap/internal/slices"
+	. "github.com/Ullaakut/nmap/internal/slices"
 )
 
 // ScanRunner represents something that can run a scan.
