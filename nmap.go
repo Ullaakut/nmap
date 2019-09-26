61535873f0d0d9afa270359a54843c3ae8a46d8b
Jed Frey <jed-frey@users.noreply.github.com>
2019-09-26 18:54:58 -0400

    Update nmap.go

--- nmap.go
+++ nmap.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	. "github.com/Ullaakut/nmap/internal/slices"
+	. "github.com/ullaakut/nmap/internal/slices"
 )
 
 // ScanRunner represents something that can run a scan.
