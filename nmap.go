ebdb752920bedfb3392646a54bbd882006b80ed0
Terminal <32599364+TheSecEng@users.noreply.github.com>
2019-10-05 12:52:34 -0400

    Update nmap.go
    Co-Authored-By: Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>

--- nmap.go
+++ nmap.go
@@ -1166,7 +1166,7 @@ func WithDebugging(level int) func(*Scanner) {
 		if level < 0 || level > 10 {
 			panic("value given to nmap.WithDebugging() should be between 0 and 10")
 		}
-		s.args = append(s.args, "-d")
+		s.args = append(s.args, fmt.Sprintf("-d%d", level))
 		s.args = append(s.args, fmt.Sprintf("%d", level))
 	}
 }
