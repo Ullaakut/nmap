24df89d95f5657a47b9ce30268bd67b2d095755d
Elias Flötzinger <elias.floetzinger@telekom.de>
2020-09-22 14:56:19 +0200

    Change function WithPorts
    This change will avoid error "only 1 -p option allowed"

--- nmap.go
+++ nmap.go
@@ -670,8 +670,23 @@ func WithPorts(ports ...string) func(*Scanner) {
 	portList := strings.Join(ports, ",")
 
 	return func(s *Scanner) {
-		s.args = append(s.args, "-p")
-		s.args = append(s.args, portList)
+		//find if any port set
+		var place int = -1
+		for p, value := range s.args {
+			if value == "-p" {
+				place = p
+				break
+			}
+		}
+
+		//add ports
+		if place > 0 {
+			portList = portList + "," + s.args[place+1]
+			s.args[place+1] = portList
+		} else {
+			s.args = append(s.args, "-p")
+			s.args = append(s.args, portList)
+		}
 	}
 }
 
