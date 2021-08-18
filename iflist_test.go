1d7f5736a8457eb680892c9a8ba9595d9ff06d88
Elias Flötzinger <eliasfloetzinger@gmx.de>
2021-08-18 10:21:25 +0200

    Simplify iflist tests by using testify
    Co-authored-by: Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>

--- iflist_test.go
+++ iflist_test.go
@@ -1,33 +1,43 @@
 package nmap
 
 import (
+	"github.com/stretchr/testify/assert"
+	"net"
 	"testing"
 )
 
 func TestScanner_GetInterfaceList(t *testing.T) {
 	scanner, err := NewScanner(WithBinaryPath("tests/scripts/fake_nmap_iflist.sh"))
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
 
 	result, err := scanner.GetInterfaceList()
-	if err != nil {
-		t.Error(err)
-	}
 
-	if len(result.Interfaces) != 2 {
-		t.Error("There should be 2 interfaces inside")
-	}
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
 
-	if len(result.Routes) != 2 {
-		t.Error("There should be 2 routes inside")
-	}
+	assert.Len(t, result.Interfaces, 2)
+	assert.Len(t, result.Routes, 2)
 }
 
 func TestConvertInterface(t *testing.T) {
-	i := convertInterface("lo     (lo)     127.0.0.1/8                               loopback down 65536")
+	i := convertInterface("lo     (lo)     127.0.0.1/8                               loopback down 65536 11:11:11:11:11:11")
 
-	if i.Up {
-		t.Error("Interface should be down")
-	}
+	assert.Equal(t, "lo", i.Device)
+	assert.Equal(t, "(lo)", i.Short)
+	assert.Equal(t, net.ParseIP("127.0.0.1"), i.IP)
+	assert.Equal(t, net.ParseIP("255.0.0.0").To4(), i.IPMask)
+	assert.Equal(t, "loopback", i.Type)
+	assert.False(t, i.Up)
+	assert.Equal(t, 65536, i.MTU)
+	assert.Equal(t, net.HardwareAddr{0x11,0x11,0x11,0x11,0x11,0x11}, i.Mac)
+}
+
+func TestConvertRoute(t *testing.T) {
+	r := convertRoute("192.168.0.0/24                            wlp5s0 600 192.168.0.1")
+
+	assert.Equal(t, net.ParseIP("192.168.0.0"), r.DestinationIP)
+	assert.Equal(t, net.ParseIP("255.255.255.0").To4(), r.DestinationIPMask)
+	assert.Equal(t, "wlp5s0", r.Device)
+	assert.Equal(t, 600, r.Metric)
+	assert.Equal(t, net.ParseIP("192.168.0.1"), r.Gateway)
 }
